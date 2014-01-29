0ff4d01b7ecccdaa832208988f5121a1625ffca2
Shao-Ping Lee <slee168@illinois.edu>
2014-01-29 00:20:27 -0600

    error handling

--- youbike_collector.py
+++ youbike_collector.py
@@ -13,7 +13,8 @@ def get_current_data():
         data = json.loads(content)['retVal']
     except ValueError:
         print("Error loading json")
-
+        print("Resp: %s\nContent: %s" % (resp, content))
+        return None
     return data
 
 file_exists = os.path.isfile('youbike_data.csv')
@@ -26,16 +27,17 @@ csv_writer = csv.writer(out_csv)
 
 for i in range(12 * 24):
 	data = get_current_data()
-	if not file_exists:
-		header = ['time'] + [dictionary['sno'] for dictionary in data]
-		csv_writer.writerow(header)
-		file_exists = True
-	row = [dictionary['sbi'] for dictionary in data]
-	print("Data for %s" % data[0]['mday'][:-2])
-	row = [data[0]['mday'][:-2]] + row
-	csv_writer.writerow(row)
+    if not data == None:
+        if not file_exists:
+            header = ['time'] + [dictionary['sno'] for dictionary in data]
+            csv_writer.writerow(header)
+            file_exists = True
+        row = [dictionary['sbi'] for dictionary in data]
+        print("Data for %s" % data[0]['mday'][:-2])
+        row = [data[0]['mday'][:-2]] + row
+        csv_writer.writerow(row)
 
-	time.sleep(300)
+        time.sleep(300)
 out_csv.close()
 
 print("Done")
